7d4572d2be58375436b181cfdeb7a04bda36d885
Sean Ruda <seanruda4@gmail.com>
2023-09-16 12:09:44 -0400

    Timer functionality

--- src/Pages/competitions.js
+++ src/Pages/competitions.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 const Button = styled.button`
@@ -20,27 +20,58 @@ const Competitions = () => {
                 This is the Competitions Page for PeteCode!
             </h1>
             <a href="https://leetcode.com/problems/two-sum/description/" target="_blank" rel="noreferrer">
-                <Button> Link To Problem </Button>
+                <Button onClick={Timer}> Link To Problem </Button>
             </a>
         </div>
     );
 };
 
 function Timer() {
-    const [time, setTime] = useState(0)
-    return (
-        <>
-        <h1>Time</h1>
-        <div>
-            <span></span>
+    // state to store time
+  const [time, setTime] = useState(0);
 
+  // state to check stopwatch running or not
+  const [isRunning, setIsRunning] = useState(false);
 
-        </div>
-        </>
-    )
-}
+  useEffect(() => {
+    let intervalId;
+    if (isRunning) {
+      // setting time from 0 to 1 every 10 milisecond using javascript setInterval method
+      intervalId = setInterval(() => setTime(time + 1), 10);
+    }
+    return () => clearInterval(intervalId);
+  }, [isRunning, time]);
 
+  // Hours calculation
+  const hours = Math.floor(time / 360000);
 
+  // Minutes calculation
+  const minutes = Math.floor((time % 360000) / 6000);
 
+  // Seconds calculation
+  const seconds = Math.floor((time % 6000) / 100);
+
+  // Milliseconds calculation
+  const milliseconds = time % 100;
+
+  // Method to start and stop timer
+  const startAndStop = () => {
+    setIsRunning(!isRunning);
+  };
+
+  // Method to reset timer back to 0
+  const reset = () => {
+    setTime(0);
+  };
+  return (
+    <div className="stopwatch-container">
+      <p className="stopwatch-time">
+        {hours}:{minutes.toString().padStart(2, "0")}:
+        {seconds.toString().padStart(2, "0")}:
+        {milliseconds.toString().padStart(2, "0")}
+      </p>
+     </div>
+  );
+};
 
 export default Competitions;
