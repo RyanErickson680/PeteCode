771e047405760c1f7f0688d694b2f3b3a00c80a6
Ryan Erickson <erirya29@gmail.com>
2023-09-16 19:08:45 -0400

    random link getting

--- src/Pages/competitions/BtnComponent.js
+++ src/Pages/competitions/BtnComponent.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RandomProblemsList from '../randomProblem'
 
-let links = RandomProblemsList(-1, "Array", 1)
 
-// Get the random LeetCode problem link
-const randomLink = links[0];
-const linkElement = document.getElementById("random-link");
-linkElement.href = randomLink;
+
+
 
 function BtnComponent(props) {
+
+  const [randomLink, setRandomLink] = useState('');
+
+  useEffect(() => {
+    console.log("getting link");
+    async function fetchRandomLink() {
+      const links = await RandomProblemsList(1, "Array", 1);
+      if (links && links.length > 0) {
+        setRandomLink(links[0]);
+      }
+    }
+
+    fetchRandomLink();
+  }, []);
   return (
     <div>
-        <a id = "random-link" href = "" target="_blank">
+        <a id = "random-link" href={randomLink} target="_blank">
           <button className="stopwatch-btn stopwatch-btn-gre" onClick={props.start}>Start</button> 
         </a>
       
--- src/Pages/randomProblem.js
+++ src/Pages/randomProblem.js
@@ -1,31 +1,23 @@
-import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import CategoryDropdown from './categoryDropdown';
-import { diff } from 'semver';
 
 /**
- * 
  * @param {int} difficulty integer value from 1 to 3
- * @param {String} category 
- * @param {int} numQuestions 
- * @returns {String[]}
+ * @param {String} category
+ * @param {int} numQuestions
+ * @returns {Promise<Array>}
  */
-function RandomProblemsList(difficulty, category, numQuestions) {
-  const [problems, setProblems] = useState([]);
-  var randomProblems;
-
+async function RandomProblemsList(difficulty, category, numQuestions) {
   // Function to filter problems by category
   const filterByCategory = (data, category) => {
     return data.filter(problem => problem.stat.question__title.includes(category));
   };
 
-
   const filterByDifficulty = (data, difficulty) => {
-    if(difficulty == -1) {
-        return data;
+    if (difficulty === -1) {
+      return data;
     }
-    return data.filter(problem => problem.difficulty.level == difficulty.toLowerCase())
-  }
+    return data.filter(problem => problem.difficulty.level === difficulty.toString());
+  };
 
   // Function to get random problems
   const getRandomProblems = (data, count) => {
@@ -33,30 +25,17 @@ function RandomProblemsList(difficulty, category, numQuestions) {
     return shuffledData.slice(0, count);
   };
 
-  const fetchRandomProblems = async () => {
-    try {
-      const response = await axios.get('https://leetcode.com/api/problems/algorithms/');
-      const data = response.data.stat_status_pairs;
-      const categoryProblems = filterByCategory(data, category);
-      const difficultyProblems = filterByDifficulty(categoryProblems, difficulty);
-      randomProblems = getRandomProblems(difficultyProblems, numQuestions); 
-      setProblems(randomProblems);
-    } catch (error) {
-      console.error('Error fetching problems:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchRandomProblems();
-  }, [category]);
-
- // const handleCategoryChange = (event) => {
-   // setSelectedCategory(event.target.value);
-  //};
-
-  return (
-    randomProblems
-  );
+  try {
+    const response = await axios.get('https://leetcode.com/api/problems/algorithms/');
+    const data = response.data.stat_status_pairs;
+    const categoryProblems = filterByCategory(data, category);
+    const difficultyProblems = filterByDifficulty(categoryProblems, difficulty);
+    const randomProblems = getRandomProblems(difficultyProblems, numQuestions);
+    return randomProblems;
+  } catch (error) {
+    console.error('Error fetching problems:', error);
+    throw error; // Propagate the error
+  }
 }
 
 export default RandomProblemsList;
