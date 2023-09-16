f1325e9311cbe250e5de23bad89d3bf027416189
Sean Ruda <seanruda4@gmail.com>
2023-09-16 12:46:58 -0400

    added timer functionality

--- src/Pages/competitions.js
+++ src/Pages/competitions.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { useState, useEffect } from 'react';
-
+import Stopwatch from "./stopwatch";
 
 const Button = styled.button`
             background-color: black;
@@ -20,58 +19,11 @@ const Competitions = () => {
                 This is the Competitions Page for PeteCode!
             </h1>
             <a href="https://leetcode.com/problems/two-sum/description/" target="_blank" rel="noreferrer">
-                <Button onClick={Timer}> Link To Problem </Button>
+                <Stopwatch />
             </a>
         </div>
     );
 };
 
-function Timer() {
-    // state to store time
-  const [time, setTime] = useState(0);
-
-  // state to check stopwatch running or not
-  const [isRunning, setIsRunning] = useState(false);
-
-  useEffect(() => {
-    let intervalId;
-    if (isRunning) {
-      // setting time from 0 to 1 every 10 milisecond using javascript setInterval method
-      intervalId = setInterval(() => setTime(time + 1), 10);
-    }
-    return () => clearInterval(intervalId);
-  }, [isRunning, time]);
-
-  // Hours calculation
-  const hours = Math.floor(time / 360000);
-
-  // Minutes calculation
-  const minutes = Math.floor((time % 360000) / 6000);
-
-  // Seconds calculation
-  const seconds = Math.floor((time % 6000) / 100);
-
-  // Milliseconds calculation
-  const milliseconds = time % 100;
-
-  // Method to start and stop timer
-  const startAndStop = () => {
-    setIsRunning(!isRunning);
-  };
-
-  // Method to reset timer back to 0
-  const reset = () => {
-    setTime(0);
-  };
-  return (
-    <div className="stopwatch-container">
-      <p className="stopwatch-time">
-        {hours}:{minutes.toString().padStart(2, "0")}:
-        {seconds.toString().padStart(2, "0")}:
-        {milliseconds.toString().padStart(2, "0")}
-      </p>
-     </div>
-  );
-};
 
 export default Competitions;
