6287127306868622472e221220532a466031ee4d
Ryan Erickson <erirya29@gmail.com>
2023-09-17 08:02:27 -0400

    better navbar

--- src/Components/Navbar/navbarElements.js
+++ src/Components/Navbar/navbarElements.js
@@ -28,17 +28,42 @@ align-items: center;
 text-decoration: none;
 padding: 0 1rem;
 height: 100%;
-cursor: pointer;
-&.active {
-	color: #808080;
-	font-weight: bold;
-}
+font-size: 18px;
 &.HoverLink {
-	&:hover {
-		font-weight: bold;
-		mix-blend-mode: multiply;
-    	transform: scale(1.1);
-	}
+  color: #edda8b;
+  font-family: inherit;
+  font-weight: 800;
+  cursor: pointer;
+  position: relative;
+  border: none;
+  background: none;
+  text-transform: uppercase;
+  transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
+  transition-duration: 400ms;
+  transition-property: color;
+  &:focus,
+  &:hover {
+  color: #808080;
+  }
+
+  &:focus:after,
+  &:hover:after {
+  width: 100%;
+  left: 0%;
+}
+&:after {
+	content: "";
+	pointer-events: none;
+	bottom: -2px;
+	left: 50%;
+	position: absolute;
+	width: 0%;
+	height: 3px;
+	background-color: #808080;
+	transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
+	transition-duration: 400ms;
+	transition-property: width, left;
+  }
 }
 `;
 
