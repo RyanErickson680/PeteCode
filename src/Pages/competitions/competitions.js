9141dea805eba90ca23fc60a9c2a50a6a4cb4b03
Sean Ruda <seanruda4@gmail.com>
2023-09-17 03:22:10 -0400

    CSS styling

--- src/Pages/competitions/competitions.js
+++ src/Pages/competitions/competitions.js
@@ -62,6 +62,8 @@ function Competitions() {
     <div className="main-section">
      <div className="clock-holder">
           <div className="stopwatch">
+               <h1>Daily Competition!</h1>
+               <h4>Click start to begin the timer and stop once it's solved</h4>
                <DisplayComponent time={time}/>
                <BtnComponent status={status} stop={stop} start={start}/>
           </div>
