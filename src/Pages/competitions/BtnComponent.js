a3fa02a5e35cd210ab97f2b072460cee8e8f037e
Sean Ruda <seanruda4@gmail.com>
2023-09-16 23:59:45 -0400

    fixed BtnComponent issues

--- src/Pages/competitions/BtnComponent.js
+++ src/Pages/competitions/BtnComponent.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import RandomProblemsList from '../randomProblem'
+import React from 'react';
 import GetData from "../board.js"
 import getFinalTime from "./competitions.js";
 import { stop, Competitions } from "./competitions.js";
@@ -7,7 +6,14 @@ import { stop, Competitions } from "./competitions.js";
 let problemOfTheDay = "https://leetcode.com/problems/shortest-path-visiting-all-nodes/?envType=daily-question&envId=2023-09-17"
 
 function BtnComponent(props) {
-    return (
+  const checkSolved = (randomLink, recentSubmissionList) => {
+    if (recentSubmissionList.includes(randomLink)) {
+      return "Solved";
+    } else {
+      return "Not Solved";
+    }
+  };
+  return (
     <div>
         <a id = "random-link" href = {problemOfTheDay} target="_blank" rel="noreferrer">
           <button className="stopwatch-btn stopwatch-btn-gre" onClick={props.start}>Start</button> 
@@ -18,22 +24,10 @@ function BtnComponent(props) {
           <button className="stopwatch-btn stopwatch-btn-red"
                   onClick={() => {
                     props.stop();
-                    
                     checkSolved(problemOfTheDay, GetData().recentSubmissionList);}}>Stop</button>
         </div> : ""
-      } */
+      } 
     </div>
-  );
-}
-
-function checkSolved(randomLink, recentSubmissionList) {
-  if (recentSubmissionList.includes(randomLink)) {
-      const finalTime = this.getFinalTime()
-  } else {
-    const finalTime = ""
-  }
-  return (
-    <h1>finalTime</h1>
   );
 }
 
