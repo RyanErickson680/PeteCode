019b728bcedbf5892aaaf77a0d444267b0bd7479
Ayushsinghal05 <ayushisawesome7@gmail.com>
2023-09-17 04:39:39 -0400

    user submissions

--- src/Pages/competitions/BtnComponent.js
+++ src/Pages/competitions/BtnComponent.js
@@ -22,7 +22,8 @@ function BtnComponent(props) {
             className="stopwatch-btn stopwatch-btn-red"
             onClick={async () => {
               props.stop();
-              const result = await checkSolved(problemOfTheDay);
+              const currname = await getUserUsername();
+              const result = await GetData(currname,problemOfTheDay);
               console.log(result);
             }}
           >
@@ -33,31 +34,10 @@ function BtnComponent(props) {
         ""
       )}
     </div>
-  );
+  ); 
 }
-const checkSolved = async (problemOfTheDay) => {
-  try {
-    const temp_username = await getUserUsername();
-    const response = await GetData(temp_username);
-    if (response) {
-      const recentSubmissionList = response.recentSubmissionList;
-      const titles = recentSubmissionList.title;
-      console.log(titles);
-      if (titles.includes(problemOfTheDay)) {
-        const finalTime = getFinalTime()
-        console.log(finalTime)
-        return "Solved";
-      } else {
-        return "Not Solved";
-      }
-    } else {
-      return "Data not available"; // Handle the case where data is not available
-    }
-  } catch (error) {
-    console.error(error);
-    return "Error"; // Handle the error gracefully
-  }}
-  async function GetData(currname) {
+
+  async function GetData(currname,problemOfTheDay) {
     
     // Define the endpoint and headers
     const url = "/graphql";
@@ -93,7 +73,7 @@ const checkSolved = async (problemOfTheDay) => {
         }
       `,
       variables: {
-        username: username,
+        username: currname,
       },
     };
   
@@ -123,29 +103,7 @@ const checkSolved = async (problemOfTheDay) => {
         ).count;
         const recentSubmissionList = responseData.data.recentSubmissionList;
         const name_solved = recentSubmissionList.map(item => item.title);
-
-        // Format and print output
-        return (name_solved);
-      } else {
-        throw new Error(`Error ${response.status}: ${response.statusText}`);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-
-    try {
-      // Make the POST request
-      const response = await fetch(url, {
-        method: "POST",
-        headers: headers,
-        body: JSON.stringify(data),
-      });
-  
-      if (response.ok) {
-        const responseData = await response.json();
-  
-        // Return the entire response object
-        return responseData;
+        return name_solved;
       } else {
         throw new Error(`Error ${response.status}: ${response.statusText}`);
       }
