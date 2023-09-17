13cbcb27409cec5980406dfb04b250fe702c1ee3
Sean Ruda <seanruda4@gmail.com>
2023-09-16 23:43:44 -0400

    fixed BtnComponent

--- src/Pages/competitions/BtnComponent.js
+++ src/Pages/competitions/BtnComponent.js
@@ -1,24 +1,29 @@
-import RandomProblemsList from '../randomProblem';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import RandomProblemsList from '../randomProblem'
+import GetData from "../board.js"
+import getFinalTime from "./competitions.js";
+import { stop, Competitions } from "./competitions.js";
+
+let problemOfTheDay = "https://leetcode.com/problems/shortest-path-visiting-all-nodes/?envType=daily-question&envId=2023-09-17"
 
 function BtnComponent(props) {
-  return (
+    return (
     <div>
-        <a id="random-link" href={"https://leetcode.com/problems/shortest-path-visiting-all-nodes/?envType=daily-question&envId=2023-09-17"} target="_blank">
-          <button className="stopwatch-btn stopwatch-btn-gre" onClick={props.start}>
-            Start
-          </button>
+        <a id = "random-link" href = {problemOfTheDay} target="_blank" rel="noreferrer">
+          <button className="stopwatch-btn stopwatch-btn-gre" onClick={props.start}>Start</button> 
         </a>
 
-      {props.status === 1 ? (
+      {(props.status === 1)? 
         <div>
-          <button className="stopwatch-btn stopwatch-btn-red" onClick={props.stop}>
-            Stop
-          </button>
-        </div>
-      ) : null}
+          <button className="stopwatch-btn stopwatch-btn-red"
+                  onClick={() => {
+                    props.stop();
+                    
+                    checkSolved(problemOfTheDay, GetData().recentSubmissionList);}}>Stop</button>
+        </div> : ""
+      } */
     </div>
-    );
+  );
 }
 
 function checkSolved(randomLink, recentSubmissionList) {
