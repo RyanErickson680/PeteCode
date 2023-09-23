566540d3302197f032b4e10c0ad9f27b4e7c3585
Ryan Erickson <erirya29@gmail.com>
2023-09-22 20:38:16 -0400

    random headers ig

--- src/Pages/recommendations.js
+++ src/Pages/recommendations.js
@@ -156,8 +156,12 @@ async function GetData(name) {
     // Define the endpoint and headers
     const url = "https://leetcode.com/graphql";
     const headers = {
-        "Content-Type": "application/json"//,
+        "Content-Type": "application/json",//,
         //"User-Agent": "Mozilla/5.0",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Methods" : "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        "Access-Control-Allow-Headers" : "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
     };
 
     // Define the query and variables
